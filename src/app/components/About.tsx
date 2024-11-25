ac36c2330d14f5e0ae8c20a680291160da871891
kusaline214 <dreamdumper@gmail.com>
2024-11-25 22:37:42 +0900

    a

---
+++ src/app/components/About.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h3 className="font-bold text-4xl text-center mt-[100px]">
+        エンジニアが自分の意思で成長していける
+        <br />
+        そんな会社を目指しています。
+      </h3>
+
+      <h4 className="font-bold text-3xl text-center mt-[30px]">News</h4>
+      <div className="text-bold text-xl border-2 border-solid border-black rounded-2xl md:w-[800px] md:mx-auto my-3 p-10">
+        <ul className="list-disc">
+          <li>〇〇へ参画しました</li>
+          <li>〇〇へ参画しました</li>
+          <li>〇〇へ参画しました</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default About;
