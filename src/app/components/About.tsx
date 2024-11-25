b6cb6d66371c7f1dc969ab670cf2af1169d8375a
kusaline214 <dreamdumper@gmail.com>
2024-11-25 23:11:51 +0900

    経営理念追加

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -3,13 +3,14 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h3 className="font-bold text-4xl text-center mt-[100px]">
+      <h2 className="font-bold text-4xl text-center mt-[100px]">経営理念</h2>
+      <h3 className="font-bold text-3xl text-center mt-[30px]">
         エンジニアが自分の意思で成長していける
         <br />
         そんな会社を目指しています。
       </h3>
 
-      <h4 className="font-bold text-3xl text-center mt-[30px]">News</h4>
+      <h4 className="font-bold text-2xl text-center mt-[30px]">News</h4>
       <div className="text-bold text-xl border-2 border-solid border-black rounded-2xl md:w-[800px] md:mx-auto my-3 p-10">
         <ul className="list-disc">
           <li>〇〇へ参画しました</li>
--- src/app/components/Career.tsx
+++ src/app/components/Career.tsx
@@ -1,7 +1,63 @@
+import {
+  Badge,
+  Code,
+  DataList,
+  Flex,
+  IconButton,
+  Link,
+} from "@radix-ui/themes";
+import { CopyIcon } from "@radix-ui/react-icons";
 import React from "react";
 
 const Career = () => {
-  return <div>Career</div>;
+  return (
+    <div>
+      <DataList.Root>
+        <DataList.Item align="center">
+          <DataList.Label minWidth="88px">Status</DataList.Label>
+          <DataList.Value>
+            <Badge color="jade" variant="soft" radius="full">
+              Authorized
+            </Badge>
+          </DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">ID</DataList.Label>
+          <DataList.Value>
+            <Flex align="center" gap="2">
+              <Code variant="ghost">u_2J89JSA4GJ</Code>
+              <IconButton
+                size="1"
+                aria-label="Copy value"
+                color="gray"
+                variant="ghost"
+              >
+                <CopyIcon />
+              </IconButton>
+            </Flex>
+          </DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Name</DataList.Label>
+          <DataList.Value>Vlad Moroz</DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Email</DataList.Label>
+          <DataList.Value>
+            <Link href="mailto:vlad@workos.com">vlad@workos.com</Link>
+          </DataList.Value>
+        </DataList.Item>
+        <DataList.Item>
+          <DataList.Label minWidth="88px">Company</DataList.Label>
+          <DataList.Value>
+            <Link target="_blank" href="https://workos.com">
+              WorkOS
+            </Link>
+          </DataList.Value>
+        </DataList.Item>
+      </DataList.Root>
+    </div>
+  );
 };
 
 export default Career;
